dd04379dce54d70d4d1d68f1b58806415a61be27
sooryasrajan <soorya.s27@gmail.com>
2024-10-08 23:58:07 +0530

    Added templates

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import StartingPage from "./pages/starting-page";
+import GamePage from "./pages/game-page";
+import Results from "./pages/results";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <StartingPage/>,
+    },
+    {
+        path: "/game",
+        element: <GamePage/>
+    },
+    {
+        path: "/results",
+        element: <Results/>
+    }
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
